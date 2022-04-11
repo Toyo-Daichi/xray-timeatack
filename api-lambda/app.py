61511daf5f68be508f95d24e70d8f50ec0da15fa
Toyo-Daichi <daichi.toyooka.gu@gmail.com>
2022-04-11 17:44:57 +0900

    add: basic lambda

--- api-lambda/app.py
+++ api-lambda/app.py
@@ -1,29 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created from 2022.04.08
+@author: Toyo_Daichi
+"""
+
 from chalice import Chalice
 
-app = Chalice(app_name='xray-apigateway-lambda')
+"""config"""
+app = Chalice(app_name='xray-api-lambda')
 
+"""Main"""
+@app.route('/hello')
+def main():
+    return {'messages': 'Hello world!'}
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/hello2')
+def main():
+    return {'messages': 'Hello world!!'}
---
+++ cli/mk_api.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Created on 2022.04.11
+
+echo 'Prepared $chalice new-project function_name'
+#
+chome=${HOME}/xray
+export AWS_ACCOUNT_ID=`aws sts get-caller-identity | jq '.Account' | sed 's/"//g' `
+
+#----------------------------------------------------------------------
+# +++ Set configure
+#----------------------------------------------------------------------
+sysname='xray'
+type='api-lambda'
+app_name=${sysname}-${type}
+config=${chome}/${type}/.chalice/config.json
+stage='dev'
+status='update' # 'create' or 'update'
+
+cat << EOF > ${config}
+{
+  "version": "2.0",
+  "app_name": "${app_name}",
+  "stages": {
+    "${stage}": {
+      "manage_iam_role": false,
+      "iam_role_arn": "arn:aws:iam::${AWS_ACCOUNT_ID}:role/lambda_basic_execution",
+      "lambda_timeout": 15,
+      "xray": true
+    }
+  }
+}
+EOF
+
+#----------------------------------------------------------------------
+# +++ Execution
+#----------------------------------------------------------------------
+cd ${chome}/${type}
+if [ ${status} == 'create' ]; then
+  pip install -r requirements.txt
+fi
+#
+chalice deploy --stage ${stage}
+
+echo 'Normal END'
+
+exit
